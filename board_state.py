c02a47e913575c348a896f442ecdb25e883d454c
lesprgm <lesozzy5@gmail.com>
2025-06-16 15:00:36 -0400

    Implemented cell rendering

--- board_state.py
+++ board_state.py
@@ -9,18 +9,31 @@ def dead_state(width, height):
 
 def random_state(width,height):
     cell_state = dead_state(width,height)
-    for x in range(height):
-        for y in range(width):
+    for y in range(height):
+        for x in range(width):
             random_number = random.random()
             if random_number >= 0.5:
                 cell_state[y][x] = 1
             else:
                 cell_state[y][x] = 0
     return cell_state
+
+def render(board_state):
+    height = len(board_state)
+    width = len(board_state[0])
+    print("-" * (width + 2))
     
-width = 5
-height = 5
-initial_dead_board = random_state(width, height)
-print("Random Board:")
-for row in initial_dead_board:
-    print(row)
+    for y in range(height):
+        row_string = "|"
+        for x in range(width):
+            cell = board_state[y][x]
+            if cell == 1:
+                row_string += "#"
+            else:
+                row_string += " "
+        row_string += "|"
+        print(row_string)
+        
+    print("-" * (width + 2))
+
+    
