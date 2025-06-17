647637b4ffdf5dab4e577817033b223c6f9f0d4f
lesprgm <lesozzy5@gmail.com>
2025-06-17 13:07:59 -0400

    Added next board state function

--- game_of_life.py
+++ game_of_life.py
@@ -35,5 +35,51 @@ def render(board_state):
         print(row_string)
         
     print("-" * (width + 2))
+    
+def get_board_dimensions (board):
+    height = len(board)
+    if height == 0:
+        return 0, 0
+    width = len(board[0])
+    return width, height
 
+def next_board_state(initial_state):
+    width, height = get_board_dimensions(initial_state)
+    
+    new_state = dead_state(width, height)
+    
+    neighbor_offsets = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),           (0, 1),
+        (1, -1), (1, 0), (1, 1)
+    ]
+    
+    for y in range(height):
+        for x in range(width):
+            current_cell_value = initial_state[y][x]
+            live_neighbors = 0
+            
+            for dy, dx in neighbor_offsets:
+                neighbor_y, neighbor_x = y + dy, x + dx
+                
+                if 0 <= neighbor_y < height and 0 <= neighbor_x < width:
+                    if initial_state[neighbor_y][neighbor_x] == 1:
+                        live_neighbors += 1
+                        
+            if current_cell_value == 1:
+                if live_neighbors < 2:
+                    new_state[y][x] = 0
+                elif live_neighbors == 2 or live_neighbors == 3:
+                    new_state[y][x] = 1
+                else:
+                    new_state[y][x] = 0
+            
+            else:
+                if live_neighbors == 3:
+                    new_state[y][x] = 1
+                else:
+                    new_state[y][x] = 0
+    return new_state
+                
+    
     
