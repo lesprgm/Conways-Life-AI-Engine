b6859a96073172c8ebf74317e9ed0d65ea9efe47
lesprgm <lesozzy5@gmail.com>
2025-06-25 19:27:40 -0400

    Adjusted genetic algorithm parameters for improved performance (I will continue adjusting this)

--- ga_parameters.py
+++ ga_parameters.py
@@ -1,10 +1,10 @@
-ga_population_size = 5
-ga_num_generations = 3
+ga_population_size = 100
+ga_num_generations = 500
 ga_mutation_rate = 0.05
-ga_simulation_steps = 10 
-ga_board_width = 5
-ga_board_height = 5
-ga_fitness_threshold = 100
+ga_simulation_steps = 500 
+ga_board_width = 20
+ga_board_height = 20
+ga_fitness_threshold = 501
 ga_crossover_rate = 0.8   
 ga_tournament_size = 3       
    
