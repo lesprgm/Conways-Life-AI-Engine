88fd9e40b2b1c17230b5d9bca99a97e72bef5ecd
lesprgm <lesozzy5@gmail.com>
2025-06-26 11:43:18 -0400

    Adjusted genetic algorithm parameters for mutation rate and tournament size(I will continue adjusting this till I see an overall upward trend)

--- ga_parameters.py
+++ ga_parameters.py
@@ -1,12 +1,12 @@
 ga_population_size = 100
 ga_num_generations = 500
-ga_mutation_rate = 0.05
+ga_mutation_rate = 0.01
 ga_simulation_steps = 500 
 ga_board_width = 20
 ga_board_height = 20
 ga_fitness_threshold = 501
 ga_crossover_rate = 0.8   
-ga_tournament_size = 3       
+ga_tournament_size = 5       
    
  
 
