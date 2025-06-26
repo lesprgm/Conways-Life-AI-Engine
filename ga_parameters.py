c1e85fef07790494e23b547d6a2f2a878ceeaec2
lesprgm <lesozzy5@gmail.com>
2025-06-26 15:26:49 -0400

    Adjust genetic algorithm parameters for simulation steps and fitness threshold

--- ga_parameters.py
+++ ga_parameters.py
@@ -1,10 +1,10 @@
 ga_population_size = 100
 ga_num_generations = 500
 ga_mutation_rate = 0.01
-ga_simulation_steps = 500 
+ga_simulation_steps = 2000 
 ga_board_width = 20
 ga_board_height = 20
-ga_fitness_threshold = 501
+ga_fitness_threshold = 2001
 ga_crossover_rate = 0.8   
 ga_tournament_size = 5       
    
